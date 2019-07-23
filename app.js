968f338919337d5eb0598d420892e2783a107ee6
Peter Ayeni <ayenicology@gmail.com>
2019-07-23 18:36:17 +0100

    UI: improve ui

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
-seedDb()
+//seedDb()
 /* // Create new Camp Ground
 Campground.create({
     name: "Waley Camp",
